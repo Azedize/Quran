f4083aea586a4b1b099e5231ac5a13757033a2e5
azedine chentouf <azedinechentouf0@gmail.com>
2024-05-15 16:21:20 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import MainContextProvider from './Context/MainContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <MainContextProvider>
     <App />
     </MainContextProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
